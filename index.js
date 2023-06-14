49768f085d35a8b34630df1b37459669f2074493
md-abir-alahe <mdabiralahe@gmail.com>
2023-06-14 23:17:58 +0600

    make user admin and install JWT

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
@@ -54,6 +55,23 @@ async function run() {
             const result = await usersCollection.insertOne(user);
             res.send(result);
         })
+
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const filter = {_id : new ObjectId(id)};
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                },
+            };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
+
+
+
+
         // menu related apis
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
