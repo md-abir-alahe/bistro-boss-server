4a55842e67c194759f0a0664c4e6270d9541da6b
md-abir-alahe <mdabiralahe@gmail.com>
2023-06-14 14:36:19 +0600

    save user if he doesn't exists in the database

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=> {
+app.get('/', (req, res) => {
     res.send('Boss is sitting');
 })
 
 // mongo db start
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.lxz9tc9.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lxz9tc9.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -31,12 +31,72 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const database = client.db("bistroDb");
+        const usersCollection = database.collection("users");
+        const menuCollection = database.collection("menu");
+        const reviewCollection = database.collection("reviews");
+        const cartCollection = database.collection("carts");
+
+        // users related apis
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
+            res.send(result);
+        })
+
+        app.post('/users', async(req, res) => {
+            const user = req.body;
+            const query = {email: user.email}
+            const existingUser = await usersCollection.findOne(query);
+            if(existingUser){
+                return res.send({message: 'user already exists'})
+            }
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+        // menu related apis
+        app.get('/menu', async (req, res) => {
+            const result = await menuCollection.find().toArray();
+            res.send(result);
+        })
+        // review related apis
+        app.get('/reviews', async (req, res) => {
+            const result = await reviewCollection.find().toArray();
+            res.send(result);
+        })
+        // cart collection api's
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            if (!email) {
+                res.send([]);
+            }
+                const query = { email: email };
+                const result = await cartCollection.find(query).toArray();
+                res.send(result);
+            
+        })
+
+        // cart collection
+        app.post('/carts', async (req, res) => {
+            const item = req.body;
+            console.log(item);
+            const result = await cartCollection.insertOne(item);
+            res.send(result);
+        })
+
+        app.delete('/carts/:id', async(req, res)=>{
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)};
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
@@ -46,6 +106,20 @@ run().catch(console.dir);
 
 
 
-app.listen(port, ()=> {
-    console.log(`Bistro Boss is sitting on port ${port}`)  ;
+app.listen(port, () => {
+    console.log(`Bistro Boss is sitting on port ${port}`);
 })
+
+/* 
+    ---------------------
+        Naming Convention
+    ---------------------
+    users : userCollection
+    app.get('/users')
+    app.get('/users/:id'')
+    app.post('/users')
+    app.patch('/users/:id')
+    app.put('/users/:id')
+    app.delete('/users/:id')
+
+*/
